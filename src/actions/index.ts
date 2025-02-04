234ce56065fb6db73399ec3994c29c89a7bd3569
francesco <aliatis.sf@gmail.com>
2025-02-04 11:06:40 -0500

    project with turso

---
+++ src/actions/index.ts
@@ -0,0 +1,26 @@
+import { loginUser, logout, registerUser } from './auth';
+import { loadProductsFromCart } from './cart/load-products-from-cart.action';
+import { createUpdateProduct } from './products/create-update-product.action';
+import { deleteProductImage } from './products/detelete-product-image-.action';
+import { getProductBySlug } from './products/get-product-by-slug.action';
+import { getProductsByPage } from './products/get-products-by-page.action';
+
+export const server = {
+  // actions
+
+  // Auth
+  loginUser,
+  logout,
+  registerUser,
+
+  // Products
+  getProductsByPage,
+  getProductBySlug,
+  createUpdateProduct,
+  deleteProductImage,
+
+  //cart
+  loadProductsFromCart,
+
+  //admin
+};
